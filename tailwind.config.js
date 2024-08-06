a3d99dcea495e7e55d43648a63ddf20cd7b8e8ba
sanderrhalv <sandeh2110@gmail.com>
2024-08-06 12:32:19 +0200

    prøvd på media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,24 @@
 module.exports = {
   content: ["./public/**/*.html", "./public/**/*.js"],
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 480px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+
     extend: {},
   },
   plugins: [],
 }
-
